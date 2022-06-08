1b70dff0f89bcfd7c977064893eaddd2a75c006b
Orlikoff <studyorlik@gmail.com>
2022-06-08 19:19:10 +0300

    Added procfile and reqs

--- teafy/settings.py
+++ teafy/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h=%y^nx8+msz(__@k5nn0k!iv!rj3qm^ew$)lh&d0ymc*+ac9@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['teafy-app.herokuapp.com', '127.0.0.1']
 AUTH_USER_MODEL = 'blog.Profile'
 LOGOUT_REDIRECT_URL = '/info'
 LOGIN_URL = '/login'
